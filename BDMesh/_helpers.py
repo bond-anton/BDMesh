cc4d8fb4eeb3d0ca2e4aa8950b69eb319ba9cf49
bond_anton <bond.anton@gmail.com>
2017-07-26 02:37:34 +0300

    tests for helper function check if integer. Unnecessary debug stdout output deleted.

--- BDMesh/_helpers.py
+++ BDMesh/_helpers.py
@@ -2,12 +2,10 @@ from __future__ import division, print_function
 import math as m
 
 
-def check_if_integer(x, threshold=1e-10, debug=False):
+def check_if_integer(x, threshold=1e-10):
     lower = m.floor(x)
     upper = m.ceil(x)
     closest = lower if abs(lower - x) < abs(upper - x) else upper
-    if debug:
-        print(x, lower, upper, closest, closest - x)
     if abs(closest - x) < threshold:
         return True
     else:
---
+++ tests/test_helpers.py
@@ -0,0 +1,16 @@
+from BDMesh._helpers import check_if_integer
+
+import unittest
+
+class TestMesh1D(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_check_if_integer(self):
+        self.assertTrue(check_if_integer(1, 1e-10))
+        self.assertTrue(check_if_integer(1.0, 1e-10))
+        self.assertTrue(check_if_integer(1.0 - 3e-11, 1e-10))
+        self.assertTrue(check_if_integer(1.0 + 3e-11, 1e-10))
+        self.assertFalse(check_if_integer(1.0 - 1e-10, 1e-10))
+        self.assertFalse(check_if_integer(1.0 + 1e-10, 1e-10))
