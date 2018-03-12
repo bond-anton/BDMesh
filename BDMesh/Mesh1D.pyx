c5c1474f58b7ad53b16b847b750208ba69fb2739
bond_anton <bond.anton@gmail.com>
2018-03-12 23:59:43 +0300

    Sync.

--- BDMesh/Mesh1D.pyx
+++ BDMesh/Mesh1D.pyx
@@ -5,11 +5,22 @@ cdef class Mesh1D(object):
     cdef:
         double physical_boundary_1, physical_boundary_2
         double boundary_condition_1, boundary_condition_2
+        double[:] local_nodes
+        double[:] solution
+        double[:] residual
 
     def __cinit__(self, double physical_boundary_1, double physical_boundary_2,
                   double boundary_condition_1, double boundary_condition_2):
-        self.physical_boundary_1 = physical_boundary_1
-        self.physical_boundary_2 = physical_boundary_2
-        self.boundary_condition_1 = boundary_condition_1
-        self.boundary_condition_2 = boundary_condition_2
+        if physical_boundary_1 < physical_boundary_2:
+            self.physical_boundary_1 = physical_boundary_1
+            self.physical_boundary_2 = physical_boundary_2
+            self.boundary_condition_1 = boundary_condition_1
+            self.boundary_condition_2 = boundary_condition_2
+        else:
+            self.physical_boundary_2 = physical_boundary_1
+            self.physical_boundary_1 = physical_boundary_2
+            self.boundary_condition_2 = boundary_condition_1
+            self.boundary_condition_1 = boundary_condition_2
 
+    def __str__(self):
+        return 'Mesh1D: [%2.2g; %2.2g], %d nodes' % (self.physical_boundary_1, self.physical_boundary_2, 2)
--- demo/Mesh1D_demo.py
+++ demo/Mesh1D_demo.py
@@ -2,7 +2,7 @@ from __future__ import division, print_function
 from BDMesh.Mesh1D import Mesh1D
 
 
-MyMesh = Mesh1D(0.0, 1.0, 0.0, 0.0)
-
+MyMesh = Mesh1D(0.0, 10.0, 0.0, 0.0)
 
 print(MyMesh)
+print(MyMesh.solution)
