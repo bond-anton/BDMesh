d614d6e78336c6bd7b1652a2a663aa408aed34f6
bond_anton <bond.anton@gmail.com>
2018-03-13 17:57:53 +0300

    syncing work...

--- BDMesh/Mesh1D.pyx
+++ BDMesh/Mesh1D.pyx
@@ -3,24 +3,35 @@ from __future__ import division, print_function
 
 cdef class Mesh1D(object):
     cdef:
-        double physical_boundary_1, physical_boundary_2
-        double boundary_condition_1, boundary_condition_2
-        double[:] local_nodes
-        double[:] solution
-        double[:] residual
+        double __physical_boundary_1, __physical_boundary_2
+        double __boundary_condition_1, __boundary_condition_2
+        double[:] __local_nodes
+        double[:] __solution
+        double[:] __residual
 
     def __cinit__(self, double physical_boundary_1, double physical_boundary_2,
                   double boundary_condition_1, double boundary_condition_2):
         if physical_boundary_1 < physical_boundary_2:
-            self.physical_boundary_1 = physical_boundary_1
-            self.physical_boundary_2 = physical_boundary_2
-            self.boundary_condition_1 = boundary_condition_1
-            self.boundary_condition_2 = boundary_condition_2
+            self.__physical_boundary_1 = physical_boundary_1
+            self.__physical_boundary_2 = physical_boundary_2
+            self.__boundary_condition_1 = boundary_condition_1
+            self.__boundary_condition_2 = boundary_condition_2
         else:
-            self.physical_boundary_2 = physical_boundary_1
-            self.physical_boundary_1 = physical_boundary_2
-            self.boundary_condition_2 = boundary_condition_1
-            self.boundary_condition_1 = boundary_condition_2
+            self.__physical_boundary_2 = physical_boundary_1
+            self.__physical_boundary_1 = physical_boundary_2
+            self.__boundary_condition_2 = boundary_condition_1
+            self.__boundary_condition_1 = boundary_condition_2
+
+    @property
+    def physical_boundary_1(self):
+        return self.__physical_boundary_1
+
+    @physical_boundary_1.setter
+    def physical_boundary_1(self, physical_boundary_1):
+        if self.__physical_boundary_2 is None or self.__physical_boundary_2 > float(physical_boundary_1):
+            self.__physical_boundary_1 = float(physical_boundary_1)
+        else:
+            raise ValueError('physical boundary 2 must be greater than physical boundary 1')
 
     def __str__(self):
-        return 'Mesh1D: [%2.2g; %2.2g], %d nodes' % (self.physical_boundary_1, self.physical_boundary_2, 2)
+        return 'Mesh1D: [%2.2g; %2.2g], %d nodes' % (self._physical_boundary_1, self._physical_boundary_2, 2)
