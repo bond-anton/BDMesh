6de30cdabf46978c574ae421978080fdfbda002c
bond_anton <bond.anton@gmail.com>
2018-07-14 01:40:30 +0300

    Version 0.2.0

--- BDMesh/__init__.py
+++ BDMesh/__init__.py
@@ -1,5 +1,4 @@
 from __future__ import division, print_function
-from BDMesh.Mesh1D_py import Mesh1D_py
 from BDMesh.Mesh1D import Mesh1D
 from BDMesh.Mesh1DUniform import Mesh1DUniform
 from BDMesh.TreeMesh1D import TreeMesh1D
--- setup.py
+++ setup.py
@@ -48,13 +48,14 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 
     keywords='Mesh',
 
     packages=find_packages(exclude=['demo', 'tests', 'docs', 'contrib', 'venv']),
     ext_modules=cythonize('BDMesh/*.pyx'),
-    install_requires=['numpy'],
+    install_requires=['numpy', 'Cython'],
     test_suite='nose.collector',
     tests_require=['nose']
 )
