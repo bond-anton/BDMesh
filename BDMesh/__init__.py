f0f64dd0074c00674f0bd019ca69ac46be4d607f
bond_anton <bond.anton@gmail.com>
2018-07-14 13:21:32 +0300

    __init__ fixes

--- BDMesh/__init__.py
+++ BDMesh/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import division, print_function
-from BDMesh.Mesh1D import Mesh1D
-from BDMesh.Mesh1DUniform import Mesh1DUniform
-from BDMesh.TreeMesh1D import TreeMesh1D
-from BDMesh.TreeMesh1DUniform import TreeMesh1DUniform
+from .Mesh1D import Mesh1D
+from .Mesh1DUniform import Mesh1DUniform
+from .TreeMesh1D import TreeMesh1D
+from .TreeMesh1DUniform import TreeMesh1DUniform
